76e443f0a7bc57dd1770d4c27e45aa044435c6ee
seno9jp <seno.kazu@gmail.com>
2022-12-30 21:21:07 +0900

    add #05 Transaction

--- src/block.rs
+++ src/block.rs
@@ -1,13 +1,14 @@
 use std::fmt::{ self, Debug, Formatter };
+use crate::transaction::Transaction;
 use super::*;
 
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
-    pub hash: BlockHash,
-    pub prev_block_hash: BlockHash,
+    pub hash: Hash,
+    pub prev_block_hash: Hash,
     pub nonce: u64,
-    pub payload: String,
+    pub transactions: Vec<Transaction>,
     pub difficulty: u128,
 }
 
@@ -17,22 +18,22 @@ impl Debug for Block {
             &self.index,
             &hex::encode(&self.hash),
             &self.timestamp,
-            &self.payload,
+            &self.transactions.len(),
             &self.nonce,
         )
     }
 }
 
 impl Block {
-    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash,
-                nonce: u64, payload: String, difficulty: u128) -> Self {
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: Hash,
+                transactions: Vec<Transaction>, difficulty: u128) -> Self {
         Block {
             index,
             timestamp,
             hash: vec![0, 32],
             prev_block_hash,
-            nonce,
-            payload,
+            nonce: 0,
+            transactions,
             difficulty,
         }
     }
@@ -57,14 +58,19 @@ impl Hashable for Block {
         bytes.extend(&u128_bytes(&self.timestamp));
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
-        bytes.extend(self.payload.as_bytes());
+        bytes.extend(
+            self.transactions
+                .iter()
+                .flat_map(|transaction| transaction.bytes())
+                .collect::<Vec<u8>>()
+        );
         bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
     }
 }
 
-pub fn check_difficulty (hash: &BlockHash, difficulty: u128) -> bool {
+pub fn check_difficulty (hash: &Hash, difficulty: u128) -> bool {
     difficulty > difficulty_bytes_as_u128(&hash)
 }
 
